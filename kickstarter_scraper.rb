48e581eaa44c8b19d364abf5465210720f1f29ad
Rachel Wyatt <rw126@evansville.edu>
2018-10-07 22:27:29 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,9 +1,14 @@
 # require libraries/modules here
-
+require 'pry'
 def create_project_hash
 
   html = File.read('fixtures/kickstarter.html')
 
   kickstarter = Nokogiri::HTML(html)
+  binding.pry
   # write your code here
 end
+# projects: kickstarter.css("li.project.grid_4")
+# title: project.css("h2.bbcard_name strong a").text
+# image link: project.css("div.project-thumbnail a img").attribute("src").value
+# description: project.css("p.bbcard_blurb").text
